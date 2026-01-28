5b6c7d956576522de848261e735e7d0b864e0279
Priest <priestdev767@gmail.com>
2026-01-28 12:13:58 +0100

    Fix wallet logos - use direct official wallet logo URLs from repositories

--- dashboard/includes/wallet_logo_cache.php
+++ dashboard/includes/wallet_logo_cache.php
@@ -6,15 +6,15 @@
 
 $walletLogos = [
     'metamask' => 'https://upload.wikimedia.org/wikipedia/commons/3/36/MetaMask_Fox.svg',
-    'trust-wallet' => 'https://avatars.githubusercontent.com/u/24244296?s=200&v=4',
-    'coinbase' => 'https://avatars.githubusercontent.com/u/18732972?s=200&v=4',
-    'phantom' => 'https://pbs.twimg.com/profile_images/1505143332707299332/_CVhNp6n_400x400.jpg',
-    'ledger' => 'https://avatars.githubusercontent.com/u/6879968?s=200&v=4',
-    'okx' => 'https://avatars.githubusercontent.com/u/67220159?s=200&v=4',
-    'trezor' => 'https://avatars.githubusercontent.com/u/4419015?s=200&v=4',
-    'exodus' => 'https://avatars.githubusercontent.com/u/20166188?s=200&v=4',
-    'argent' => 'https://avatars.githubusercontent.com/u/48281670?s=200&v=4',
-    'myetherwallet' => 'https://avatars.githubusercontent.com/u/16744597?s=200&v=4'
+    'trust-wallet' => 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x0000000000085d4780B73119b8B580991DEe8d52/logo.png',
+    'coinbase' => 'https://images.ctfassets.net/c5bd0wqwc5v0/5mRJMxJWqKgQE9Y0QJJVi/cf5a7f0b82e1dd62a8c006f9c3f2d4d3/coinbase-logo.png',
+    'phantom' => 'https://raw.githubusercontent.com/phantom/brand-assets/main/png/Phantom%20Logo%20-%20Purple.png',
+    'ledger' => 'https://raw.githubusercontent.com/ledgerhq/ledger-live/develop/apps/ledger-live-desktop/public/icon.png',
+    'okx' => 'https://www.okx.com/cdn/assets/imgs/202305/logo.png',
+    'trezor' => 'https://trezor.io/static/images/trezor-logo.png',
+    'exodus' => 'https://raw.githubusercontent.com/ExodusMovement/exodus-desktop/master/public/assets/icon.png',
+    'argent' => 'https://raw.githubusercontent.com/argentlabs/argent-x/main/packages/extension/public/argent.svg',
+    'myetherwallet' => 'https://raw.githubusercontent.com/MyEtherWallet/MyEtherWallet/master/public/images/logo.png'
 ];
 
 /**
